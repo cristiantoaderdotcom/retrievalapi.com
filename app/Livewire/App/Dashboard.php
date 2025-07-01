212705e040165d7bdf8db62189e6540826e42ce0
Cristian Toader <toader.cristian93@gmail.com>
2025-07-02 00:58:04 +0300

    cleaning db

--- app/Livewire/App/Dashboard.php
+++ app/Livewire/App/Dashboard.php
@@ -23,64 +23,7 @@ class Dashboard extends Component {
 	];
 
 	public function mount(Request $request) {
-		$cacheKey = 'dashboard_stats_' . $request->user()->id;
-		$this->data = cache()->remember($cacheKey, 600, function() use ($request) {
-			$this->stats['inboxes'] = EmailInbox::query()->where('user_id', $request->user()->id)->count();
-			$this->stats['messages_limit'] = Number::abbreviate($request->user()->messages_limit);
-			$this->stats['context_limit'] = Number::abbreviate($request->user()->context_limit);
-
-			$inboxIds = EmailInbox::query()
-				->where('user_id', $request->user()->id)
-				->pluck('id');
 		
-			$this->stats['total_emails'] = ProcessedEmail::query()
-				->whereIn('email_inbox_id', $inboxIds)
-				->count();
-			
-			$this->stats['total_replies'] = ProcessedEmail::query()
-				->whereIn('email_inbox_id', $inboxIds)
-				->where('was_replied', true)
-				->count();
-			
-			$this->stats['reply_rate'] = $this->stats['total_emails'] > 0 
-				? round(($this->stats['total_replies'] / $this->stats['total_emails']) * 100, 2) 
-				: 0;
-
-			$stats = [];
-			for ($i = 29; $i >= 0; $i--) {
-				$date = Carbon::now()->subDays($i)->format('Y-m-d');
-				$startOfDay = Carbon::parse($date)->startOfDay();
-				$endOfDay = Carbon::parse($date)->endOfDay();
-				
-				$emails = ProcessedEmail::query()
-					->whereIn('email_inbox_id', $inboxIds)
-					->whereDate('created_at', $date)
-					->count();
-				
-				$replies = ProcessedEmail::query()
-					->whereIn('email_inbox_id', $inboxIds)
-					->where('was_replied', true)
-					->whereBetween('created_at', [$startOfDay, $endOfDay])
-					->count();
-				
-				$stats[] = [
-					'date' => $date,
-					'emails' => $emails ?? 0,
-					'replies' => $replies ?? 0,
-				];
-			}
-			
-			return $stats;
-		});
-
-		$shouldRedirect = !$request->user()->pro && ($request->user()->last_pro_reminder_at === null || Carbon::parse($request->user()->last_pro_reminder_at)->lt(Carbon::now()->subDays(5)));
-		if ($shouldRedirect) {
-			$request->user()->update([
-				'last_pro_reminder_at' => now(),
-			]);
-
-			return redirect()->route('app.account.plans');
-		}
 	}
 
 	public function render(Request $request) {
